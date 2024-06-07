028678dad089e7701383657476b7251b82241647
Santiago Gelvez <santiagogelvez29@gmail.com>
2024-06-06 20:54:17 -0500

    Setting: local port changed

--- bank_manager/bank_manager/settings.py
+++ bank_manager/bank_manager/settings.py
@@ -138,5 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'app_accounts.CustomUser'
 
 # CORS
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5174"]
 CORS_ALLOW_CREDENTIALS = True
